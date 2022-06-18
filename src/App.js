45ccceb3360db66f8d1863ea3f1f18efd4689e7c
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-06-18 15:35:55 -0300

    feat: simple todo exemple

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "green",
+    textDecoration: "line-through",
+  };
+
+  const [todo, setTodos] = useState([
+    {
+      text: "Learn React",
+      completed: false,
+    },
+    {
+      text: "Learn TypeScript",
+      completed: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    // check if the text is empty
+    if (text === "") {
+      alert("Please enter a todo");
+    } else {
+      setTodos([...todo, { text, completed: false }]);
+    }
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todo];
+    newTodos[index].completed = true;
+    setTodos(newTodos);
+  };
+
+  const uncompleteTodo = (index) => {
+    const newTodos = [...todo];
+    newTodos[index].completed = false;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todo];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="addTodo"/>
+      <button onClick={() => addTodo(document.getElementById("addTodo").value)}>
+        {" "}
+        Add{" "}
+      </button>
+
+      <ul>
+        {todo.map((todo, index) => (
+          <li key={index} style={todo.completed ? completedStyle : null}>
+            {todo.text}
+
+            {todo.completed ? (
+              <button onClick={() => uncompleteTodo(index)}>Uncomplete</button>
+            ) : (
+              <button onClick={() => completeTodo(index)}>Complete</button>
+            )}
+
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
